86802db75b3c1be432f76c76fdd0be94d5c3a2f5
Andy <935065201@qq.com>
2018-10-22 18:49:28 +0800

    公共文件开发

--- src/views/CommonFile.vue
+++ src/views/CommonFile.vue
@@ -3,15 +3,16 @@
     <bg-title title="公共文件"></bg-title>
     <padding-wrapper>
       <search-wrapper>
-        <search-bar :searchForm='selectParams' @search-file="search" @download-file="download" :role='role' @reset-form-file="resetSearch"></search-bar>
+        <search-bar :searchForm='selectParams' @search-file="search" :role='role' @reset-form-file="resetSearch"></search-bar>
       </search-wrapper>
       <mini-wrapper>
         <div class="betweenLayout">
           <div>
           </div>
           <div>
-            <el-button size="mini" type="primary" @click="$router.push({name:'Upload'})" icon="el-icon-plus">批量删除</el-button>
-            <el-button size="mini" type="primary" @click="$router.push({name:'Upload'})" icon="el-icon-plus">上传文件</el-button>
+            <el-button size="mini" type="primary" icon="el-icon-plus" @click="_downloadFile({type:'multiple',id:multipleSelection})">批量下载</el-button>
+            <el-button v-if="role === 'admin'" size="mini" type="primary" icon="el-icon-plus">批量删除</el-button>
+            <el-button v-if="role === 'admin'" size="mini" type="primary" @click="$router.push({name:'Upload'})" icon="el-icon-plus">上传文件</el-button>
           </div>
         </div>
       </mini-wrapper>
@@ -25,22 +26,22 @@
           </template>
           <el-input>12</el-input>
         </el-table-column>
-        <el-table-column align="center" header-align="center" prop="file_original_name" :formatter="formatterType" label="文件类型"></el-table-column>
+        <el-table-column align="center" header-align="center" prop="file_type" :formatter="formatterType" label="文件类型"></el-table-column>
         <el-table-column align="center" header-align="center" prop="upload_time" label="上传时间"></el-table-column>
         <el-table-column align="center" header-align="center" prop="file_size" :formatter="formatterFileSize" label="文件大小"></el-table-column>
-        <el-table-column align="center" header-align="center" prop="file_size" :formatter="formatterFileSize" label="上传人"></el-table-column>
+        <el-table-column align="center" header-align="center" prop="user_name" label="上传人"></el-table-column>
         <el-table-column align="center" header-align="center" prop="download_count" label="下载次数"></el-table-column>
         <el-table-column align="center" header-align="center" prop="remark" label="备注"></el-table-column>
         <el-table-column v-if="role === 'admin'" align="center" header-align="center" prop="status" :formatter="formatterStatus" label="状态"></el-table-column>
         <el-table-column align="center" header-align="center" label="操作">
           <template slot-scope="scope">
             <el-button v-if="role ==='admin'" type="text" @click="_adminDeleteFile({type: 'waste',id:scope.row.id})">删除</el-button>
-            <!-- <span v-if="role ==='user'" style="color:#409EFF">|</span>
-            <el-button v-if="role ==='user'" type="text" @click="_adminDeleteFile({type: 'waste',id:scope.row.id})">垃圾箱</el-button> -->
             <span v-if="role ==='admin'" style="color:#409EFF">|</span>
             <el-button type="text" @click="_downloadFile({type:'single',id:scope.row.id,name:scope.row.file_original_name})">下载</el-button>
+            <span v-if="role === 'admin'" style="color:#409EFF">|</span>
+            <el-button v-if="role === 'admin'" type="text" @click="_getDownloadRecord(scope.row.id)">下载记录</el-button>
             <span style="color:#409EFF">|</span>
-            <el-button type="text" @click="_getDownloadRecord(scope.row.id)">下载记录</el-button>
+            <el-button type="text" @click="_getDownloadRecord(scope.row.id)">详情</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -146,13 +147,6 @@ export default {
       };
       this.selectAdminOrUser();
     },
-    download() {
-      if (this.multipleSelection.length > 0) {
-        this._downloadFile({ type: 'multiple', id: this.multipleSelection });
-      } else {
-        this.$message({ message: '请选择要操作条目', type: 'warning' });
-      }
-    },
     // 下载文件方法（下载一条或者是多条）
     _downloadFile(data) {
       let id = '';
@@ -173,32 +167,31 @@ export default {
         _download = this.$api.downFile({ fileId: id });
       }
 
-      _download
-        .then((res) => {
-          this.loadSign = false;
-          const blob = new Blob([res]);
-          let fileName = '未命名.rar';
-          if (data.type === 'multiple') {
-            fileName = '批量文件.zip';
-          } else {
-            fileName = data.name;
-          }
-          if ('download' in document.createElement('a')) {
-            // 非IE下载
-            const elink = document.createElement('a');
-            elink.download = fileName;
-            elink.style.display = 'none';
-            elink.href = URL.createObjectURL(blob);
-            document.body.appendChild(elink);
-            elink.click();
-            URL.revokeObjectURL(elink.href); // 释放URL 对象
-            document.body.removeChild(elink);
-          } else {
-            // IE10+下载
-            navigator.msSaveBlob(blob, fileName);
-          }
-          this.$message.success('下载成功');
-        })
+      _download.then((res) => {
+        this.loadSign = false;
+        const blob = new Blob([res]);
+        let fileName = '未命名.rar';
+        if (data.type === 'multiple') {
+          fileName = '批量文件.zip';
+        } else {
+          fileName = data.name;
+        }
+        if ('download' in document.createElement('a')) {
+          // 非IE下载
+          const elink = document.createElement('a');
+          elink.download = fileName;
+          elink.style.display = 'none';
+          elink.href = URL.createObjectURL(blob);
+          document.body.appendChild(elink);
+          elink.click();
+          URL.revokeObjectURL(elink.href); // 释放URL 对象
+          document.body.removeChild(elink);
+        } else {
+          // IE10+下载
+          navigator.msSaveBlob(blob, fileName);
+        }
+        this.$message.success('下载成功');
+      })
         .catch(() => {
           this.loadSign = false;
           this.$message.warning('下载出错！');
@@ -208,43 +201,31 @@ export default {
     _adminDeleteFile(params) {
       const _delete = this.$api.deleteManageUploadFile;
       if (params.type === 'delete') {
-        this.$confirm('此操作将删除该文件, 是否继续?', '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning',
-        }).then(() => {
-          _delete({ fileId: params.id, targetStatus: 0 }).then(
-            (res) => {
-              console.log(res);
-              if (res.code === '200') {
-                this.selectAdminOrUser();
-                this.$message.success('操作成功！！！');
-              }
-            },
-            () => {
-              this.$message.warning('操作出错了！');
-            },
-          );
-        });
+        _delete({ fileId: params.id, targetStatus: 0 }).then(
+          (res) => {
+            console.log(res);
+            if (res.code === '200') {
+              this.selectAdminOrUser();
+              this.$message.success('操作成功！！！');
+            }
+          },
+          () => {
+            this.$message.warning('操作出错了！');
+          },
+        );
       } else if (params.type === 'waste') {
-        this.$confirm('此操作将该删除该文件, 是否继续?', '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning',
-        }).then(() => {
-          _delete({ fileId: params.id }).then(
-            (res) => {
-              console.log(res);
-              if (res.code === '200') {
-                this.selectAdminOrUser();
-                this.$message.success('操作成功！！！');
-              }
-            },
-            () => {
-              this.$message.warning('操作出错了！');
-            },
-          );
-        });
+        _delete({ fileId: params.id }).then(
+          (res) => {
+            console.log(res);
+            if (res.code === '200') {
+              this.selectAdminOrUser();
+              this.$message.success('操作成功！！！');
+            }
+          },
+          () => {
+            this.$message.warning('操作出错了！');
+          },
+        );
       }
     },
     // 获取文件下载记录列表
@@ -274,12 +255,7 @@ export default {
       }
     },
     formatterType(row) {
-      const index = row.file_original_name.lastIndexOf('.');
-      if (index !== -1) {
-        return row.file_original_name.slice(index + 1).toUpperCase();
-      } else {
-        return '未知';
-      }
+      return row.file_type.slice(1).toUpperCase();
     },
     formatterFileSize(row) {
       return `${(Number(row.file_size) / 1024).toFixed(1)}kb`;
